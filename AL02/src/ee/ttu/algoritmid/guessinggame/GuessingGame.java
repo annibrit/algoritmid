60e17e3cda1fd88b9524e5b68adb48efa5616ae3
annibrit <annibrit@gmail.com>
2019-09-21 22:05:43 +0300

    Tahan näha teste

--- AL02/src/ee/ttu/algoritmid/guessinggame/GuessingGame.java
+++ AL02/src/ee/ttu/algoritmid/guessinggame/GuessingGame.java
@@ -24,8 +24,11 @@ public class GuessingGame {
 
         while (correctCity.length() == 0) {
 
-            //int middleValue = (cities.size()/2) + (cities.size() % 2);
-            int middleValue = ((cities.size()+1)/2)+1;
+            int middleValue;
+            if (cities.size()==1){middleValue=1;}
+            else
+            {middleValue = (cities.size()/2) + (cities.size() % 2);}
+
 
             String answer = this.oracle.isIt(cities.get(middleValue));
             if (answer == "correct!" || cities.size()==1) {
