bf480e0d683d7c73359e2abb59b483a1d8fc020d
annibrit <annibrit@gmail.com>
2019-10-20 23:07:37 +0300

    adding

--- AL04/src/ee/ttu/algoritmid/scoreboard/ScoreBoard.java
+++ AL04/src/ee/ttu/algoritmid/scoreboard/ScoreBoard.java
@@ -17,23 +17,29 @@ public class ScoreBoard {
      * Adds a participant's time to the checkpoint scoreboard
      */
     public void add(Participant participant) {
+
+        TreeSet<Participant> participants = new TreeSet<Participant>();
+
+        participants.add(participant);
+
+        TreeSet<Participant> scoreboard = new TreeSet<>(new Comparator <Participant>() {
+            @Override
+            public int compare(Participant o1, Participant o2) {
+                if (new Integer(o1.getTime()).compareTo(new Integer(o2.getTime())) < 0)
+
+                    return -1;
+
+                else if (new Integer(o1.getTime()).compareTo(new Integer(o2.getTime())) > 0)
+
+                    return 1;
+
+                else return 0;
+            }
+        });
+
+
     }
 
-    TreeSet<Participant> participants = new TreeSet<Participant>(new Comparator<Participant>() {
-        @Override
-        public int compare(Participant o1, Participant o2) {
-            if (new Integer(o1.getTime()).compareTo(new Integer(o2.getTime())) < 0)
-
-                return -1;
-
-            else if (new Integer(o1.getTime()).compareTo(new Integer(o2.getTime())) > 0)
-
-                return 1;
-
-            else return 0;
-        }
-
-    });
 
 
     /**
