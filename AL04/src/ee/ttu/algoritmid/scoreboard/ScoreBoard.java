dbae483dfb2e2a903fdee60e4635756ee84eee64
annibrit <annibrit@gmail.com>
2019-10-20 23:13:59 +0300

    adding veel lihtsamalt

--- AL04/src/ee/ttu/algoritmid/scoreboard/ScoreBoard.java
+++ AL04/src/ee/ttu/algoritmid/scoreboard/ScoreBoard.java
@@ -1,6 +1,5 @@
 package ee.ttu.algoritmid.scoreboard;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.TreeSet;
 
@@ -22,7 +21,7 @@ public class ScoreBoard {
 
         participants.add(participant);
 
-        TreeSet<Participant> scoreboard = new TreeSet<>(new Comparator <Participant>() {
+        /*TreeSet<Participant> scoreboard = new TreeSet<>(new Comparator <Participant>() {
             @Override
             public int compare(Participant o1, Participant o2) {
                 if (new Integer(o1.getTime()).compareTo(new Integer(o2.getTime())) < 0)
@@ -35,12 +34,9 @@ public class ScoreBoard {
 
                 else return 0;
             }
-        });
-
+        });*/
 
     }
-
-
 
     /**
      * Returns top n number of participants in the checkpoint to be displayed on the scoreboard
