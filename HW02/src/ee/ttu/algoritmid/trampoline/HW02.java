74df5ad265823104cda0ac032557e84ddeaf8a2d
annibrit <annibrit@gmail.com>
2020-01-01 13:05:03 +0200

    See the tests

---
+++ HW02/src/ee/ttu/algoritmid/trampoline/HW02.java
@@ -0,0 +1,11 @@
+package ee.ttu.algoritmid.trampoline;
+
+import java.util.List;
+
+public class HW02 implements TrampolineCenter {
+
+    @Override
+    public List<String> findMinJumps(int[][] map) {
+        return null;
+    }
+}
